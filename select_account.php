a9387687d625b675be3d5938fb577fb7fb87c0d7
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2023-01-06 18:32:16 +0100

    Create templates for log up and log in + add image for backgrounds and css.

--- select_account.php
+++ select_account.php
@@ -6,10 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="assets/css/style.css" rel="stylesheet">
     </head>
-    <body class="create">
-        
-        <div class="form_create">
-            <h1 class="createh1">Welcome to the <br/>best tamagotchi app</h1>
+    <body class="select">
+        <div class="form_select">
+            <h1 class="selecth1">Welcome to the <br/>best tamagotchi app</h1>
             <h6>Created by <strong>Chris</strong>, <strong>Randa</strong> and <strong>Cloé</strong></h6>
             <form action="" method="POST">
                 <!--input type="text" name="pseudo" placeholder="Type here your own nickname"/-->
@@ -17,7 +16,7 @@
 
                 <button type="button">Register now</button>
 
-                <p>Already a member ? <a href="uc">Log to your account</a></p>
+                <p>Not a member yet ? <a href="uc">Please, create a new account</a></p>
                 
             </form>
         </div>
