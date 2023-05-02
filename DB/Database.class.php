2288866bd6f00511580264c525d1143696a3e436
Chris Centaure <c.centaure@webintelligence.fr>
2023-05-02 16:50:47 +0200

    try testing app 3

--- DB/Database.class.php
+++ DB/Database.class.php
@@ -10,7 +10,7 @@ abstract class Database
         if (!self::$pdo) {
             $config = [
                 "host" => "localhost",
-                "port" => 8889,
+                "port" => 3306,
                 "username" => "root",
                 "password" => "root",
                 "engine" => "mysql"
